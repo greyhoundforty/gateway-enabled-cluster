cf2e52ba4c4df995ef16cfa09948b2ec95bdd22f
greyhoundforty <ryantiffany@fastmail.com>
2020-07-24 15:05:57 -0500

    removed webhook for plan generation
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -10,13 +10,6 @@ resource "ibm_container_cluster" "gw_cluster" {
   private_vlan_id          = data.ibm_network_vlan.private.id
   default_pool_size        = 3
   tags                     = ["schematics-learning", var.project_name]
-
-  webhook = [{
-    level = "Normal"
-    type  = "slack"
-    url   = "https://hooks.slack.com/services/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"
-    }
-  ]
 }
 
 ## Add worker pool to other zones once I know output
