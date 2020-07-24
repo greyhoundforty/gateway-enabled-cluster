aa04e13a6eae569437c79431229d3a34b120e68a
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2020-07-24 15:09:55 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "worker_pools" {
   value = ibm_container_cluster.gw_cluster.worker_pools
 }
 
-output "worker_pool_ids" {
-  value = ibm_container_cluster.gw_cluster.worker_pools.id
-}
+# output "worker_pool_ids" {
+#   value = ibm_container_cluster.gw_cluster.worker_pools.id
+# }
